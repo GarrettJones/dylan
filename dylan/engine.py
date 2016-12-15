85b820ce816e1ee8c283ead8b44cf094f525a6c1
GarrettJones <Garrett@Garretts-MacBook-Pro.local>
2016-12-15 16:18:14 -0700

    added control variate MC to engine and lookback to payoff

--- dylan/engine.py
+++ dylan/engine.py
@@ -1,6 +1,6 @@
 import abc
 import numpy as np
-from scipy.stats import binom
+from scipy.stats import binom, norm
 
 
 class PricingEngine(object, metaclass=abc.ABCMeta):
@@ -135,3 +135,83 @@ def NaiveMonteCarloPricer(pricing_engine, option, data):
 
     return callT.mean() * disc
 
+
+def BlackScholesDelta(St, t, K, T, sig, r, div):
+    tau = T - t
+    d1 = (np.log(St/K) + (r - div + 0.5 * sig * sig) * tau) / (sig * np.sqrt(tau))
+    delta = np.exp(-div * tau) * norm.cdf(d1)
+    return delta
+    
+def BlackScholesGamma(St, t, K, T, sig, r, div):
+    tau = T - t
+    d1 = (np.log(St/K) + (r - div + 0.5 * sig * sig) * tau) / (sig * np.sqrt(tau))
+    gamma = np.exp(-div * tau ) * (norm.pdf(d1) / St * sig * np.sqrt(tau))
+    return gamma
+    
+def BlackScholesVega(St, t, K, T, sig, r, div):
+    tau = T - t
+    d2 = (np.log(St / K) + (r - div - sig * sig * 0.5) * tau)/( sig * np.sqrt(tau))
+    vega = K * np.exp(-r * tau) * norm.pdf(d2) * np.sqrt(tau)
+    return vega
+    
+def ControlVariateMonteCarloPricer(pricing_engine, option, data):
+    expiry = option.expiry
+    strike = option.strike
+    alpha = pricing_engine.alpha
+    xi = pricing_engine.xi
+    Vbar = pricing_engine.Vbar
+    (spot, rate, volatility, dividend) = data.get_data()
+    dt = expiry / pricing_engine.time_steps
+    #nudt = (rate - dividend - 0.5 * volatility * volatility) * dt
+    xisdt = xi * np.sqrt(dt)
+    erddt = np.exp((rate - dividend) * dt)
+    egam1 = np.exp(2*(rate-dividend)*dt)
+    egam2 = -2*erddt+1
+    eveg1 = np.exp(-alpha*dt)
+    eveg2 = Vbar - Vbar *eveg1
+    
+    beta1 = -1
+    beta2 = 1
+    beta3 = 1
+    
+    payoff = np.zeros((pricing_engine.replications, ))
+    price = 0.0
+
+    for j in range(pricing_engine.replications):
+        
+        cv1 = 0
+        cv2 = 0
+        cv3 = 0
+        
+        s = np.zeros(pricing_engine.time_steps)
+        v = np.zeros(pricing_engine.time_steps)
+        s[0] = spot
+        v[0] = Vbar
+        z1 = np.random.normal(size=int(pricing_engine.time_steps))
+        z2 = np.random.normal(size=int(pricing_engine.time_steps))
+        
+        for i in range(int(pricing_engine.time_steps)):
+            t = (i-1) * dt
+            delta = BlackScholesDelta(s[i], t, strike, expiry, volatility, rate, dividend)
+            gamma = BlackScholesGamma(s[i], t, strike, expiry, volatility, rate, dividend)
+            vega = BlackScholesVega(s[i], t, strike, expiry, volatility, rate, dividend)
+            
+            ##### Evolve Variance #####
+            v[i] = v[i-1] + alpha * dt + (Vbar - v[i-1]) + xisdt * z1[i]
+            if v[i] < 0.0: 
+                v[i] = 0.0
+        
+            #####Evolve Asset Price #####
+            s[i] = s[i-1] * np.exp((rate - 0.5 * v[i-1]) * dt + np.sqrt(v[i-1]) * np.sqrt(dt) * z2[i])
+            
+            ##### Accumulate Control Variates #####
+            cv1 = cv1 + delta * (s[i] - s[i-1] * erddt)
+            cv2 = cv2 + gamma * ((s[i] - s[i-1]) * (s[i] - s[i-1]) - s[i-1] * s[i-1] * (egam1 * np.exp(v[i-1]*dt) + egam2))
+            cv3 = cv3 + vega * ((v[i]- v[i-1])-(v[i] * v[i] * eveg1 + eveg2 - v[i-1]))
+
+
+        payoff[j] = option.payoff(s()) + beta1 * cv1 + beta2 * cv3 + beta3 * cv3
+
+    price = np.exp(-rate * expiry) * payoff.mean()
+    #stderr = payoff.std() / np.sqrt(engine.replications)
+    return price
--- dylan/payoff.py
+++ dylan/payoff.py
@@ -1,4 +1,5 @@
 import abc
+import numpy as np
 from numpy import maximum
 
 class Payoff(object, metaclass=abc.ABCMeta):
@@ -94,4 +95,71 @@ def put_payoff(option, spot):
 
     return maximum(option.strike - spot, 0.0)
 
+class ExoticPayoff(Payoff):
+    """
+    A plain vanilla option payoff.     
 
+    Args:
+        expiry (float):    the option's expiration date.
+        strike (int):      the option's strike price.
+        payoff (function): the option's payoff function (via the strategy pattern)
+
+    """
+
+    def __init__(self, expiry, strike, payoff):
+        self.__expiry = expiry
+        self.__strike = strike
+        self.__payoff = payoff
+
+    @property
+    def expiry(self):
+        """
+        The option's expiration date.
+
+        """
+        return self.__expiry
+
+    @expiry.setter
+    def expiry(self, new_expiry):
+        self.__expiry = new_expiry
+
+    @property
+    def strike(self):
+        """
+        The option's strike price.
+
+        Args:
+            new_Strike (float): a new strike price when setting value
+
+        """
+
+        return self.__strike
+
+    @strike.setter
+    def strike(self, new_strike):
+        self.__strike = new_strike
+
+    def payoff(self, spot):
+        return self.__payoff(self, spot)
+
+def Lookback_Call_Payoff(option, spot):
+    """
+    The payoff function for a European lookback call option. 
+
+    Args:
+        option:       the self variable from the Payoff class that aggregates the function.
+        spot (array): the price path of the underlying asset
+    """
+    
+    return maximum(np.amax(spot) - option.strike, 0.0)
+    
+def Lookback_Put_Payoff(option, spot):
+    """
+    The payoff function for a European lookback put option. 
+
+    Args:
+        option:       the self variable from the Payoff class that aggregates the function.
+        spot (array): the price path of the underlying asset
+    """
+    
+    return maximum(option.strike - np.amin(spot), 0.0)
